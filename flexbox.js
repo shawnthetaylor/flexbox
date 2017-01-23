46ffebefdae60ba29255a3c79e74d72ae54bf07c
Shawn Taylor <sxtaylor@godaddy.com>
2017-01-23 15:52:13 -0800

    Added more examples for children of flex containers

--- flexbox.js
+++ flexbox.js
@@ -27,3 +27,24 @@ function updateChildFlexGrow(e) {
 flexGrow.forEach(input => {
   input.addEventListener('change', updateChildFlexGrow);
 });
+
+const flexShrink = document.querySelectorAll('.children .flex-shrink input[type="number"]');
+
+function updateChildFlexShrink(e) {
+  document.documentElement.style.setProperty(`--flex-shrink-c${this.dataset.child}`, parseInt(this.value));
+}
+
+flexShrink.forEach(input => {
+  input.addEventListener('change', updateChildFlexShrink);
+});
+
+const alignSelf = document.querySelectorAll('.children .align-self input[type="radio"]');
+console.log(alignSelf);
+
+function updateAlignSelf(e) {
+  document.documentElement.style.setProperty('--align-self-c3', this.value);
+}
+
+alignSelf.forEach(input => {
+  input.addEventListener('change', updateAlignSelf);
+});
